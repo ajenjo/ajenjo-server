7bccf4c7037f23c3be1e1de780aa5a80935fb6a1
Jon Dotsoy <hi@jon.soy>
2015-06-05 11:47:08 -0300

    Upload dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,8 @@
 
 module.exports = function(grunt) {
 
+  // return;
+
 
 	// Load the include-all library in order to require all of our grunt
 	// configurations and task registrations dynamically.
